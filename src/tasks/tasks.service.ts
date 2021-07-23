3571a7ad4b5feeea1e0fb087ca00fff64ab36c61
Joao Sousa <joao.rsousa23@gmail.com>
2021-07-23 20:14:46 +0930

    refactor: remove console.log

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -32,8 +32,6 @@ export class TasksService {
   deleteTask(id: string): Task[] {
     this.tasks = this.tasks.filter((task) => task.id !== id);
 
-    console.log(this.tasks);
-
     return this.tasks;
   }
 }
