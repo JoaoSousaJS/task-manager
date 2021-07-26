7996d8d73677c219132bc1ed0a790875a4d19900
Joao Sousa <joao.rsousa23@gmail.com>
2021-07-26 21:06:04 +0930

    refactor: refactor updateTask controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -37,14 +37,14 @@ export class TasksController {
     return this.tasksService.createTask(createTaskDto);
   }
 
-  // @Patch('/:id/status')
-  // updateTask(
-  //   @Param('id') id: string,
-  //   @Body() updateTaskStatusDto: UpdateTaskStatusDto,
-  // ): Task {
-  //   const { status } = updateTaskStatusDto;
-  //   return this.tasksService.updateTask(id, status);
-  // }
+  @Patch('/:id/status')
+  updateTask(
+    @Param('id') id: string,
+    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+  ): Promise<Task> {
+    const { status } = updateTaskStatusDto;
+    return this.tasksService.updateTask(id, status);
+  }
 
   @Delete('/:id')
   deleteTask(@Param('id') id: string): Promise<void> {
